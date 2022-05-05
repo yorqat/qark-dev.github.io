6f7dcda2d3bee98d80ab470171b73c11c58a82f2
Qark-dev <qarkdev@gmail.com>
2022-05-05 18:16:34 +0800

    path

--- pkg/bundle.js
+++ pkg/bundle.js
@@ -263,7 +263,7 @@
     }
 
     async function main() {
-       await init('./pkg/yew_wasm_pack_minimal_bg.wasm');
+       await init('/pkg/yew_wasm_pack_minimal_bg.wasm');
        run_app();
     }
     main();
